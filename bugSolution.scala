5e814361cbc2448f285c50b23f69167c75ae6725
BugHunter-X <askzenai@gmail.com>
2024-12-20 04:56:05 +0100

    Add bugSolution.scala file

---
+++ bugSolution.scala
@@ -0,0 +1,18 @@
+```scala
+class MyClass(val x: Int) {
+  def this() = this(0) // Auxiliary constructor
+  def myMethod(): Int = {
+    if(x == 0){
+      println("x is zero")
+    }
+    x + 1
+  }
+}
+
+object Main extends App {
+  val obj = new MyClass()
+  val obj2 = new MyClass(5)
+  println(obj.myMethod()) // Output: 1
+  println(obj2.myMethod()) // Output: 6
+}
+```
